4603a2b6de49d289a0bed1a022087cc774d5c9ed
AJ ONeal <coolaj86@gmail.com>
2022-05-13 03:58:29 -0600

    WIP: tested working

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 "use strict";
 
 //@ts-ignore
-let E = require("libauth/lib/errors.js");
+//let E = require("libauth/lib/errors.js");
+let E = require("../../lib/errors.js");
 
 /**
  * @typedef GoogleOIDCOpts
  * @property {String} clientId
- * @property {String} [secretId]
+ * @property {String} [clientSecret]
  * @property {Boolean} verify
  * @property {String|Boolean} iss
  * @property {String|Boolean} exp
  * @property {String|Boolean} azp
+ * @property {String|Boolean} access_type
  * @property {Function} pluginVerify
  */
 
@@ -19,10 +21,18 @@ let E = require("libauth/lib/errors.js");
  */
 function create(userOpts) {
   let myOpts = {
+    clientId: userOpts.clientId,
+    clientSecret: userOpts.clientSecret,
     iss: userOpts.iss ?? "https://accounts.google.com",
     exp: userOpts.exp ?? true,
     claims: {
       // email_verified: userOpts.email_verified ?? true,
+    },
+    authorizationQuery: {
+      client_id: userOpts.clientId,
+      scope: "email profile",
+      //login_hint: req.query.login_hint,
+      access_type: userOpts.access_type ?? "online",
     },
     /** @param {Jws} jws */
     pluginVerify: function (jws) {
